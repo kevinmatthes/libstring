ec52e7d226d7b057e3d2a4de5b8b7e19ee2300d7
Kevin Matthes <kevin.matthes@s2019.tu-chemnitz.de>
2021-10-24 22:21:02 +0200

    Add length method

---
+++ libstring_len.c
@@ -0,0 +1,47 @@
+/**
+ * libstring_len.c : determine a string's effective count of characters.
+ *
+ * See `LICENSE' for full license.
+ * See `README.md' for project details.
+ */
+
+/**
+ * Copyright (C) 2021 Kevin Matthes
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+/******************************************************************************/
+
+/**
+ * Includes.
+ */
+
+#include "libstring.h"
+
+
+
+/**
+ * Functions.
+ */
+
+natural_t   STRING (len)    (SELF)
+{
+    natural_t   ret = 0x0;
+
+    for (ret = 0x0; self[ret]; ret++);
+
+    return ret;
+}
